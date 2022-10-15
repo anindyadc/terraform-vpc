68e9c25075c40497a8c663da506656013d5a64b2
anindyadc <anindyadc@gmail.com>
2022-10-16 03:58:21 +0530

    fixed syntax error

--- vars.tf
+++ vars.tf
@@ -14,9 +14,8 @@ variable "EC2_USER" {
   default = "ubuntu"
 }
 variable "AMI" {
-  type = "map"
-
-  default {
+  type = map
+  default = {
     eu-west-2 = "ami-03dea29b0216a1e03"
     us-east-1 = "ami-0c2a1acae6667e438"
   }
