41f638ae20d2e79f697e3654ac89fc1b1e43c10a
anindyadc <anindyadc@gmail.com>
2022-10-16 04:39:31 +0530

    AMI changed

--- vars.tf
+++ vars.tf
@@ -1,5 +1,5 @@
 variable "AWS_REGION" {
-  default = "eu-west-2"
+  default = "us-east-1"
 }
 
 variable "PRIVATE_KEY_PATH" {
@@ -17,6 +17,6 @@ variable "AMI" {
   type = map
   default = {
     eu-west-2 = "ami-03dea29b0216a1e03"
-    us-east-1 = "ami-0c2a1acae6667e438"
+    us-east-1 = "ami-08c40ec9ead489470"
   }
 }
